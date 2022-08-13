37444b7d2ced866bb0305c14146bb0397c590a3e
SJ Park <sj@SJs-MacBook-Air.local>
2022-08-12 18:45:01 -0700

    adding compiler option for C, C++, Assembler

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -10,9 +10,13 @@ set(CMAKE_SYSTEM_PROCESSOR arm) # Name of the system processor
 ####################
 # Toolchain Config #
 ####################
-set(CMAKE_C_COMPILER arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_C_COMPILER    arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER  arm-none-eabi-g++)
+set(AS                  arm-none-eabi-as)
+set(CMAKE_AR            arm-none-eabi-gcc-ar)
+set(OBJCOPY             arm-none-eabi-objcopy)
+set(OBJDUMP             arm-none-eabi-objdump)
+set(SIZE                arm-none-eabi-size)
+
 set(CMAKE_C_COMPILER_ID GNU)
 set(CMAKE_CXX_COMPILER_ID GNU)
-set(CMAKE_C_COMPILER_FORCED TRUE)
-set(CMAKE_CXX_COMPILER_FORCED TRUE)
